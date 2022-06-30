560d8d2864a27f2c2d166d59fef7a3a97a56d40e
DiyRex <75021968+DiyRex@users.noreply.github.com>
2022-06-30 13:02:26 +0530

    Update cleaner.py

--- Utils/cleaner.py
+++ Utils/cleaner.py
@@ -1,6 +1,6 @@
 import os
 
-path_list = ["Base64/decode","Base64/encode","Docs","download","QR","Local","QR_Gen","QR_ReGen","Text_to_voice"]
+path_list = ["Base64/decode","Base64/encode","Docs","download","QR","local","QR_Gen","QR_ReGen","Text_to_voice"]
 def cleaner():
     for path in path_list:
         files_list = os.listdir(path)
